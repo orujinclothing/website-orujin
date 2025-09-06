eb8c2ffc03079398152271673f72919ba4f2dd08
orujinclothing <orujin.clothing@gmail.com>
2025-09-06 20:07:53 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,8 +31,8 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
+        title={'ORUJIN CLOTHING INSANE'}
+        subtitle={'ONE OF THE CLOTHING BRANDS OF THE WORLD'}
         ctaText={'shop now'}
         ctaAction={goToShop}
       />
